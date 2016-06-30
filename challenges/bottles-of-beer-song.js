4a59feb7c8e22f23c802e02ba1d783bda604be37
cabrown91 <cleo.aliciabrown@gmail.com>
2016-06-30 14:31:02 -0700

    add for loop

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,9 @@
 */
 
 // YOUR CODE HERE
+for (i=5;i>0;i--) {
+  console.log([i]+" bottles of beer on the wall,");
+  console.log([i]+" bottles of beer!");
+  console.log("Take one down and pass it around,");
+  console.log([i-1]+" bottles of beer on the wall...");
+}
